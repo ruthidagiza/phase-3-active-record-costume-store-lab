c530fdf9a3fb8390957125a59ca7995c703e13b4
IDAGIZA <ruthidagiza@gmail.com>
2022-09-12 11:53:46 +0300

    commit tree f54f64daec6d1d6ae349febdb9c69c7451b209ea

---
+++ db/migrate/002_create_costume_stores.rb
@@ -0,0 +1,14 @@
+class CreateCostumeStores < ActiveRecord::Migration[6.1]
+    def change
+       create_table :costume_stores do |t|
+           t.string :name
+           t.string :location
+           t.integer :costume_inventory
+           t.integer :num_of_employees
+           t.datetime :opening_time
+           t.datetime :closing_time
+           t.boolean :is_in_business true
+           t.timestamps null: false
+       end
+   end
+end
